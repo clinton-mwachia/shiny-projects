8c811a1e4c9f65b4bf4de6ba91226261411c8a36
clinton moshe mwachia <clintonmwachia9@gmail.com>
2024-04-24 15:59:24 +0300

    load data, filer necessary columns, drop passengerId, convert age to integer

--- Titanic/Analysis.R
+++ Titanic/Analysis.R
@@ -1,2 +1,20 @@
 # set working directory to current folder
 setwd("F:/R/shiny-projects/Titanic")
+
+# loading libraries
+library(tidyverse)
+
+# load the data
+raw_titanic = read_csv("data/titanic.csv")
+
+# data cleaning
+# selecting only required columns
+titanic = raw_titanic |>
+  select(-starts_with("zero")) |> # dropping columns starting with zero*
+  rename(survived = `2urvived`) |> # rename 2urvived to survived
+  mutate(
+    Age = as.integer(Age) # convert age from float to integer
+  ) |> 
+  select(-Passengerid) # drop passenderId
+  
+  
